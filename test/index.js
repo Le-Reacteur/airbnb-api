8e2eec18721e7958f3135d2b4cd27e7a0dd9ad72
Farid Safi <farid.lazreq@gmail.com>
2017-03-31 11:37:26 +0200

    add tests

---
+++ test/index.js
@@ -0,0 +1,31 @@
+var chai = require("chai");
+var expect = require("chai").expect;
+var chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+
+describe("GET /", function() {
+  it("respond with welcome message", function(done) {
+    chai.request("http://localhost:3001").get("/").end(function(err, res) {
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      expect(res.text).to.equal("Welcome to the Airbnb API.");
+      done();
+    });
+  });
+});
+
+describe("GET /api/home", function() {
+  it("respond with cities and featured", function(done) {
+    chai
+      .request("http://localhost:3001")
+      .get("/api/home")
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res).to.be.json;
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property("cities").to.be.a("array");
+        expect(res.body).to.have.property("featured").to.be.a("array");
+        done();
+      });
+  });
+});
